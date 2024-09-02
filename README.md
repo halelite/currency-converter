669d27884f78e630154549a9c9d47b9a5354d145
Hale Sheikhi <hale.sheikhi81@gmail.com>
2024-09-02 20:26:06 +0330

    initial commit

--- README.md
+++ README.md
@@ -1,70 +1,63 @@
-# Getting Started with Create React App
+# Frontend Mentor - Age calculator app solution
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Welcome to the currency converter app! This app is built using React.
 
-## Available Scripts
+## Table of contents
 
-In the project directory, you can run:
+- [Overview](#overview)
+  - [Features](#features)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
 
-### `npm start`
+## Overview
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+### Features
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+Users are able to:
 
-### `npm test`
+- View the converted amount both in official and free market exchange rate after submitting a valid amount.
+- View the country flag when a currency is selected.
+- See from currency and to currency swap when switch icon is clicked
+- Receive validation errors if:
+  - input field is empty when the form is submitted
+  - input field loses focus while it's empty
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+### Screenshot
 
-### `npm run build`
+![Design preview for currency converter app](./public/design/currency-converter-screenshot.jpeg)
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+### Links
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+- Solution URL: [solution URL](https://your-solution-url.com)
+- Live Site URL: [live site URL](https://your-live-site-url.com)
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+## My process
 
-### `npm run eject`
+### Built with
 
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- [React](https://reactjs.org/) - JS library
 
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+### Continued development
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
+Currently this app only converts dollar to iranian rial and vice versa. It can be later expanded to support more currency exchanges.
 
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
+### Useful resources
 
-## Learn More
+- [ExchangeRate API](https://www.exchangerate-api.com/) - Used this API for fetching dynamic official currency exchange rates.
+- For the free market currency exchange, a static currency exchange rate is used.
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+## Author
 
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+- LinkedIn - [Hale Sheikhi](https://www.linkedin.com/in/hale-sheikhi/)
+- Frontend Mentor - [@halelite](https://www.frontendmentor.io/profile/halelite)
---
+++ src/CurrencyConverter.jsx
@@ -0,0 +1,166 @@
+import { useRef, useState } from "react";
+import switchLogo from './assets/icons/swap-svgrepo-com.svg';
+
+function CurrencyConverter() {
+
+    const amountInput = useRef('');
+    const [fromCurrency, setFromCurrency] = useState("USD");
+    const [toCurrency, setToCurrency] = useState('IRR');
+    const [freeMarketRate, setFreeMarketRate] = useState(600000);
+    const [freeMarketResult, setFreeMarketResult] = useState(0);
+    const [officialResult, setOfficialResult] = useState(0);
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [inputNumber, setInputNumber] = useState(0);
+    const [options, setOptions] = useState(['USD', 'IRR']);
+    const [flags, setFlags] = useState({
+        'USD': "./icons/united-states.svg",
+        "IRR": "./icons/iran.svg"
+    });
+
+    function handleSwitch() {
+        let temp = fromCurrency;
+        setFromCurrency(toCurrency);
+        setToCurrency(temp);
+        setOfficialResult(0);
+    }
+
+    function handleSubmit(e, amount) {
+        e.preventDefault();
+        
+        if(amount !== '') {
+            setInputNumber(amount);
+            convertCurrency(amount);
+            amountInput.current.value = '';
+        } else {
+            setError('لطفا یک مقدار وارد نمایید.');
+        }
+    }
+
+    function handleBlur(amount) {
+        if(amount === '') {
+            amountInput.current.classList.add('hasError');
+            setError('لطفا یک مقدار وارد نمایید.');
+        } else {
+            amountInput.current.classList.remove('hasError');
+            setError('');
+        }        
+    }
+
+    function convertCurrency(amount) {
+        setLoading(true);
+        fetch(`https://v6.exchangerate-api.com/v6/d153f07b9c638c53841573d8/latest/${fromCurrency}`)
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                throw Error(response.status);
+            }
+        })
+        .then(data => {
+
+            let rate = data.conversion_rates[toCurrency];
+            let result = amount * rate;
+            
+            setOfficialResult(result);
+            setLoading(false);
+        });
+
+        let officialResult;
+
+        if(fromCurrency === 'USD') {
+            officialResult = amount * freeMarketRate;
+        } else if(fromCurrency === "IRR") {
+            officialResult = amount / freeMarketRate;
+        }
+
+        setFreeMarketRate(officialResult);
+    }
+
+    const optionList = options.map(option => (
+        <option 
+        key={option}
+        value={option}
+        >
+            {option}
+        </option>
+    ))
+    
+    return (
+        <main className="converter-container">
+            <h1>مبدل ارز</h1>
+            <form>
+                <label htmlFor="amount">مبلغ *</label>
+                <input 
+                ref={amountInput} 
+                id="amount" 
+                type="number" 
+                placeholder="مبلغ مورد نظر را وارد نمایید." 
+                onBlur={() => handleBlur(amountInput.current.value)}
+                />
+                {error &&
+                <div className="error">{error}</div>
+                }
+
+                <div className="currency-inputs">
+                    <div className="select-section">
+                        <label htmlFor="from">ارز مبدأ</label>
+                        <img src={flags[fromCurrency]} alt="" />
+                        <select 
+                        id="from"
+                        value={fromCurrency} 
+                        onChange={(e) => setFromCurrency(e.target.value)}
+                        >
+                            {optionList}
+                        </select>
+                    </div>
+
+                    <div 
+                    onClick={handleSwitch}
+                    className="switch"
+                    >
+                        <img src={switchLogo} alt="switch" />
+                    </div>
+
+                    <div className="select-section">
+                        <label htmlFor="to">ارز مقصد</label>
+                        <img src={flags[toCurrency]} alt="" />
+                        <select 
+                        id="to"
+                        value={toCurrency} 
+                        onChange={(e) => setToCurrency(e.target.value)} 
+                        >
+                            {optionList}
+                        </select>
+                    </div>
+                </div>
+
+                <button 
+                className="convertBtn" 
+                type="submit" 
+                onClick={(e) => handleSubmit(e, amountInput.current.value)}
+                >
+                    تبدیل
+                </button>
+            </form>
+
+            <p>مقدار تبدیل شده:</p>
+            <div className="result-box">
+                {officialResult ? 
+                <>
+                <div className="item">
+                    <span>نرخ رسمی:</span>
+                    <p>{inputNumber} {fromCurrency} = {officialResult.toLocaleString()} {toCurrency}</p>
+                </div>
+                <div className="item">
+                    <span>نرخ بازار آزاد:</span>
+                    <p>{inputNumber} {fromCurrency} = {freeMarketRate.toLocaleString()} {toCurrency}</p>
+                </div>
+                </>
+                : loading && <div dir="ltr">Loading...</div>}
+            </div>
+        </main>
+    )
+}
+
+export default CurrencyConverter
